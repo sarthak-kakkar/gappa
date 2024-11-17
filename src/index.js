48e155b9071d449d185453f5898a5ff5d679ea38
sarthak-kakkar <sarthak.kakkar@pm.me>
2024-11-17 13:03:26 +0530

    Todo: Fix WS server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import reducers from './reducers'
+import handleNewMessage from './sagas'
+import setupSocket from './sockets'
+import username from './utils/name'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+)
+
+const socket = setupSocket(store.dispatch, username)
+
+sagaMiddleware.run(handleNewMessage, { socket, username })
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
---
+++ src/sockets/index.js
@@ -0,0 +1,31 @@
+import * as types from '../constants/ActionTypes'
+import { messageReceived, populateUsersList } from '../actions'
+
+const setupSocket = (dispatch, username) => {
+  const socket = new WebSocket('ws://localhost:3001/')
+
+  socket.onopen = () => {
+    console.log('open?');
+    socket.send(JSON.stringify({
+      type: types.ADD_USER,
+      name: username
+    }))
+  }
+  socket.onmessage = (event) => {
+    const data = JSON.parse(event.data) 
+    switch (data.type) {
+      case types.ADD_MESSAGE:
+        dispatch(messageReceived(data.message, data.author))
+        break
+      case types.USERS_LIST:
+        dispatch(populateUsersList(data.users))
+        break
+      default:
+        break
+    }
+  }
+
+  return socket
+}
+
+export default setupSocket
